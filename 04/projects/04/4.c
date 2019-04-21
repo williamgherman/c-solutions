6c315f4a07eadc256d7f2ba4a48994d36f22c1f0
JLL32 <jalalle_elhachimi@hotmail.fr>
2019-04-21 09:16:24 +0100

    Clearer precedence
    I've added parentheses to make operations precedence clearer.

--- 04/projects/04/4.c
+++ 04/projects/04/4.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-
+int main(void) 
+{
     int n;
-
-    printf("Enter a number between 0 and 32767: ");
+    
+    printf("Enter a number between 0 and 32767: "); 
     scanf("%d", &n);
-
-    printf("In octal, your number is %d%d%d%d%d\n", 
-           n / 4096 % 8,  n / 512 % 8, n / 64 % 8, n / 8 % 8, n % 8);
-
-    return 0;
+  
+    printf("In octal, your number is: %d%d%d%d%d\n", 
+           (n/4096)%8, 
+           (n/512)%8, 
+           (n/64)%8, 
+           (n/8)%8, 
+           n%8);
+    
+  return 0;
 }
