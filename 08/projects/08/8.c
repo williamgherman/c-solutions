b56f7b7a1c88e232977c2c13617cd587d3696b0d
Surepic <46145992+ioprojecton@users.noreply.github.com>
2019-12-28 17:21:04 -0500

    Update 8.c

--- 08/projects/08/8.c
+++ 08/projects/08/8.c
@@ -1,48 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+#define rows 5
+#define cols 5
 
-    int i, j, temp_total, high, low;
-    int n[5][5];
+int main()
+{
+ int array[rows][cols];
 
-    for (i = 0; i < 5; i++) {
-        printf("Enter quiz grades for student %d: ", i + 1);
+unsigned char student_total_score=0;
 
-        for (j = 0; j < 5; j++) {
-            scanf("%d", &n[i][j]);
-        }
+unsigned char col_sum=0;
+
+ for(unsigned char i=0;i<rows;i++){
+    printf("Grades for Student %d:",i+1);
+
+    unsigned char b=0;
+
+    while(b<5) scanf("%d",&array[i][b++]);
+
+ }
+putchar('\n');
+
+ for(unsigned char i=0;i<5;i++){
+printf("Total score for Student%d: ",i+1);
+
+    for(unsigned char b=0;b<5;b++){
+        student_total_score+=array[i][b];
     }
+    printf("%d ",student_total_score);
+    printf("Average Score %.2f ",(float)student_total_score/rows);
+    putchar('\n');
+    student_total_score=0;
+ }
+printf("\n\n");
 
-    printf("\n");
+ unsigned char low=255;
+ unsigned char high=0;
 
-    for (i = 0; i < 5; i++) {
-        printf("\nTotal and average score for student %d: ", i + 1);
-        temp_total = 0;
-        for (j = 0; j < 5; j++) {
-            temp_total += n[i][j];
-        }
-        printf("%d %.2f", temp_total, (double) temp_total / 5);
+ for(unsigned char i=0;i<5;i++){
+        printf("Quiz %d: ",i+1);
+
+    for(unsigned char b=0;b<5;b++){
+        col_sum+=array[b][i];
+        if(array[b][i]<low) low=array[b][i];
+        if(array[b][i]>high) high=array[b][i];
     }
+    printf("Average: %.2f Low: %d High: %d\n",(float)col_sum/cols,low,high);
 
-    printf("\n");
-
-    for (i = 0; i < 5; i++) {
-        printf("\nAverage, high and low scores for quiz %d: ", i + 1);
-        temp_total = 0;
-        high = low = n[0][i];
-
-        for (j = 0; j < 5; j++) {
-            temp_total += n[j][i];
-            if (n[j][i] > high)
-                high = n[j][i];
-            if (n[j][i] < low)
-                low = n[j][i];
-        }
-        printf("%f %d %d", (double) temp_total / 5, high, low);
-    }
-
-    printf("\n");
-
+    col_sum=0;
+ }
+ putchar('\n');
 
     return 0;
 }
