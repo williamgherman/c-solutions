e598fee6e7918fe0d5866af873655ba3a0d3cf86
Surepic <46145992+ioprojecton@users.noreply.github.com>
2019-12-28 20:58:20 -0500

    Update 11.c

--- 08/projects/11/11.c
+++ 08/projects/11/11.c
@@ -1,46 +1,81 @@
 #include <stdio.h>
 
+#define array_size 15
+
 int main(void) {
-
-    char phone[15] = {0};
-    int i = 0;
-
+    char c;
     printf("Enter phone number: ");
-    while ((phone[i++] = getchar()) != '\n')
-        ;
-    printf("In numeric form: ");
 
-    for (i = 0; i < 15; i++) {
-        switch (phone[i]) {
+    char counter=0;
+    char array[array_size];
+
+    for(unsigned char i=0;i<array_size;array[i++]=0);
+
+    while (((c = toupper(getchar())) != '\n') && (counter<array_size)) {
+
+        switch (c) {
+
             case 'A': case 'B': case 'C':
-                putchar('2');
+
+                array[counter++]='2';
+
                 break;
+
             case 'D': case 'E': case 'F':
-                putchar('3');
+
+                array[counter++]='3';
+
                 break;
+
             case 'G': case 'H': case 'I':
-                putchar('4');
+
+                array[counter++]='4';
+
                 break;
+
             case 'J': case 'K': case 'L':
-                putchar('5');
+
+                array[counter++]='5';
+
                 break;
+
             case 'M': case 'N': case 'O':
-                putchar('6');
+
+                array[counter++]='6';
+
                 break;
+
             case 'P': case 'R': case 'S':
-                putchar('7');
+
+                array[counter++]='7';
+
                 break;
+
             case 'T': case 'U': case 'V':
-                putchar('8');
+
+                array[counter++]='8';
+
                 break;
+
             case 'W': case 'X': case 'Y':
-                putchar('9');
+
+                array[counter++]='9';
+
                 break;
+
             default:
-                putchar(phone[i]);
+
+                array[counter++]=c;
+
                 break;
+
         }
+
     }
+printf("In numeric form: ");
+    for(unsigned char i=0;i<array_size;i++) printf("%c",array[i]);
     printf("\n");
+
     return 0;
+
 }
