aa83f402493c6a5d541eddf2ae2afd4854d24302
Surepic <46145992+ioprojecton@users.noreply.github.com>
2019-12-28 21:11:54 -0500

    Update 12.c

--- 08/projects/12/12.c
+++ 08/projects/12/12.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+
 #include <ctype.h>
+#define array_size 26
+
 
 int main(void) {
 
     char c;
-    int sum,
-        scrabble[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 
-                        5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 
-                        1, 4, 4, 8, 4, 10};
+
+    unsigned int sum=0;
+
+    char array[array_size]={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+
 
     printf("Enter a word: ");
 
-    while ((c = getchar()) != '\n') {
-        sum += scrabble[toupper(c) - 'A'];
-    }
+    while ((c = toupper(getchar())) != '\n') sum+=array[c-'A'];
+
     printf("Scrabble value: %d\n", sum);
+
     return 0;
 }
