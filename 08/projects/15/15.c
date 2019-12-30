c7f5ea69797fb293b169349d3ef8e85c019ed7bf
Surepic <46145992+ioprojecton@users.noreply.github.com>
2019-12-30 00:14:14 -0500

    Update 15.c

--- 08/projects/15/15.c
+++ 08/projects/15/15.c
@@ -1,33 +1,31 @@
+#include <stdbool.h>   /* C99 only */
 #include <stdio.h>
 
-#define MAX_VALUE 80
 
-int main(void) {
+#define size 80
 
-    char c, sentence[MAX_VALUE] = {0};
+int main(void)
 
-    int i, n, length;
+{
+    printf("Enter a sentence: ");
+    char array[size];
+    
+    for(unsigned char i=0;i<size;i++) array[i]=0;
 
-    printf("Enter message to be encrypted: ");
-    for (i = 0, length = 0; (c = getchar()) != '\n' && i < MAX_VALUE; i++) {
-        length++;
-        sentence[i] = c;
-    }
+    char i=0;
+    int shift=0;
+    while((array[i]=getchar())!='\n') i++;
 
     printf("Enter shift amount (1-25): ");
-    scanf("%d", &n);
+    scanf("%d",&shift);
 
-    printf("Encrypted message: ");
-    for (i = 0; i < length; i++) {
-        if (sentence[i] >= 'A' && sentence[i] <= 'Z')
-            sentence[i] = ((sentence[i] - 'A') + n) % 26 + 'A';
-        else if (sentence[i] >= 'a' && sentence[i] <= 'z')
-            sentence[i] = ((sentence[i] - 'a') + n) % 26 + 'a';
-        
-        putchar(sentence[i]);
-    }
-    printf("\n");
-
-    return 0;
+printf("Encrypted message:");
+for(unsigned char counter=0;counter<=i;counter++){
+    if(array[counter]>='A' && array[counter]<='Z') putchar(((array[counter] - 'A') + shift) % 26 + 'A');
+    else if (array[counter]>='a' && array[counter]<='z') putchar(((array[counter] - 'a') + shift) % 26 + 'a');
+    else putchar(array[counter]);
+}
+putchar('\n');
+  return 0;
 }
 
