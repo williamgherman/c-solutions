4cdcf7a5710d1d57f6b8b32b95662ed1bc8da8e3
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-01-14 20:40:44 -0500

    Update 1.c

--- 10/projects/01/1.c
+++ 10/projects/01/1.c
@@ -1,81 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h> /* exit */
-#include <stdbool.h> /* C99+ only */
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <strings.h>
 
-#define STACK_SIZE 100
+#define STACK_SIZE 4
 
-/* external variables */
-char contents[STACK_SIZE] = {0};
-int top = 0;
-
+char contents[STACK_SIZE];
+int top=0;
 void make_empty(void);
 bool is_empty(void);
 bool is_full(void);
 void push(char i);
-char pop(void);
-void stack_overflow(void);
-void stack_underflow(void);
+int pop(void);
 
-int main(void) {
+int main()
+{
+    printf("enter parenthasis: ");
+    char ch=0;
+    while(ch=getchar()){
+        if(ch=='(' || ch=='{') push(ch);
 
-    char c, d;
+else if(ch==')' && pop()!='(') printf("not nested properly");
 
-    printf("Enter parentheses and/or braces: ");
-    
-    while ((c = getchar()) != '\n') {
-        if (c == '}' && pop() != '{') {
-            printf("Parentheses/braces are not nested properly\n");
-            return 0;
-        }
-        else if (c == ')' && pop() != '(') {
-            printf("Parentheses/braces are not nested properly\n");
-            return 0;
-        } else if (c == '(' || c == '{')
-            push(c);
+else if(ch=='}' && pop()!='{')printf("not nested properly");
+
+else if(ch=='\n'){
+    if(is_empty()) {
+            printf("matched");
+    break;
     }
-    
-    if (!is_empty) {
-        printf("Parentheses/braces are not nested properly\n");
-        return 0;
+    else printf("not nested properly");
+
+}
     }
 
-    printf("Parentheses/braces are nested properly\n");
+
     return 0;
 }
-void make_empty(void) {
-    top = 0;
+
+void make_empty(void){
+top=0;
 }
 
-bool is_empty(void) {
-    return top == 0;
+bool is_empty(void){
+return top==0;
 }
 
-bool is_full(void) {
-    return top == STACK_SIZE;
+bool is_full(void){
+return top==STACK_SIZE;
 }
 
-void push(char i) {
-
-    if (is_full())
-        stack_overflow();
-    else
-        contents[top++] = i;
-}
-
-char pop(void) {
-
-    if (is_empty())
-        stack_underflow();
-    else
-        return contents[--top];
-}
-
-void stack_overflow(void) {
-    printf("\nStack overflow\n");
+void push(char i){
+if(is_full()) {
+    printf("stack overflow");
     exit(EXIT_FAILURE);
 }
 
-void stack_underflow(void) {
-    printf("\nStack underflow\n");
-    exit(EXIT_FAILURE);
+else contents[top++]=i;
+}
+
+int pop(void){
+if(is_empty()) {
+ printf("parenthasys arent mantched");
+exit(EXIT_FAILURE);
+}
+
+else return contents[--top];
 }
