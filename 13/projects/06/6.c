c5bf6f3e2cc79f02b5f6ec286ec9c58abbea943b
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-01-28 22:46:44 -0500

    Working code
    Original solution provided is not solving the problem....toupper cant be used on char *!
    
    small portion of the code modified to solve the problem

--- 13/projects/06/6.c
+++ 13/projects/06/6.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
+
 #include <string.h>
+#include <ctype.h>
+
 
 #define NUM_PLANETS 9
 
+
+
 int main(int argc, char *argv[]) {
 
+
+
     char *planets[] = {"Mercury", "Venus", "Earth",
+
                        "Mars", "Jupiter", "Saturn",
+
                        "Uranus", "Neptune", "Pluto"};
+
     int i, j;
+char tmp[32];
+char pl[32];
 
     for (i = 1; i < argc; i++) {
-        for (j = 0; j < NUM_PLANETS; j++)
-            if (strcmp(toupper(argv[i]), toupper(planets[j])) == 0) {
+
+        for (j = 0; j < NUM_PLANETS; j++){
+memset(tmp,'\0',sizeof(tmp));
+memset(pl,'\0',sizeof(pl));
+           strcpy(tmp,argv[i]);
+strcpy(pl,planets[j]);
+
+
+           for(char *b=tmp;b<tmp+strlen(tmp);b++)*b=toupper(*b);
+
+for(char *b=pl;b<pl+strlen(pl);b++)*b=toupper(*b);
+printf("%s %s\n",tmp,pl);
+            if (!strcmp(tmp, pl)) {
+
                 printf("%s is planet %d\n", argv[i], j + 1);
+
                 break;
+
             }
-        if (j == NUM_PLANETS)
-            printf("%s is not a planet\n", argv[i]);
+            }
+
+        if (j == NUM_PLANETS) printf("%s is not a planet\n", argv[i]);
+
     }
+
     return 0;
+
 }
