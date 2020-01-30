d7f41725fe1e4745c5e07136155a3e21a5479423
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-01-29 20:48:24 -0500

    same but in 1 char * array

--- 13/projects/07/7.c
+++ 13/projects/07/7.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 
     int n;
-    char *tens[] = {"twenty", "thirty", "forty", "fifty",
-                    "sixty", "seventy", "eighty", "ninety"},
-         *teens[] = {"ten", "eleven", "twelve", "thirteen", "fourteen",
-                     "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"},
-         *ones[] = {"", "-one", "-two", "-three", "-four", "-five",
-                    "-six", "-seven", "-eight", "-nine"};
+char* pointers[]={"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve", \
+"thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","fourty","fifty","sixty",\
+"seventy","eighty","ninty"
+};
 
+while(1){
     printf("Enter a two-digit number: ");
+
     scanf("%d", &n);
-    printf("You entered the number ");
-    if (n / 10 == 1)
-        printf("%s\n", teens[n % 10]);
-    else
-        printf("%s%s\n", tens[n / 10 - 2], ones[n % 10]);
+if(n<10 || n>99) {
+        printf("out of range\n");
+continue;
+}
+
+if(n<=20) printf("%s\n",pointers[n-1]);
+
+else printf("%s %s\n",pointers[n/10+18-1],pointers[n%10-1]);
+
+}
     return 0;
+
 }
