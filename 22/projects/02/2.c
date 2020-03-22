09d71caa175ea38dc83ab1edd0232c6f457c08a1
Surepic <46145992+ioprojecton@users.noreply.github.com>
2020-03-22 18:47:30 -0400

    Update 2.c

--- 22/projects/02/2.c
+++ 22/projects/02/2.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
-#include <ctype.h>  /* toupper, isalpha */
-#include <stdlib.h> /* exit */
+#include <stdlib.h>
+#include <ctype.h>
+#include <strings.h>
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    char c;
+  if(argc<2){
+    fprintf(stderr,"Too few arguments provided");
+    exit(EXIT_FAILURE);
+  }
 
+  FILE *file;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: allcaps filename\n");
-        exit(EXIT_FAILURE);
-    }
+  if(!(file=fopen(argv[1],"r"))) {
+    fprintf(stderr,"cant open %s\n",argv[1]);
+    exit(EXIT_FAILURE);
+  }
 
-    if ((fp = fopen(argv[1], "r")) != NULL)
-    {
-        while ((c = getc(fp)) != EOF)
-        {
-            if (isalpha(c))
-                putchar(toupper(c));
-            else
-                putchar(c);
-        }
-        fclose(fp);
-    }
-    exit(EXIT_SUCCESS);
+  char ch;
+  while((ch=getc(file))!=EOF) putchar(toupper(ch));
+  fclose(file);
+ return 0;
 }
