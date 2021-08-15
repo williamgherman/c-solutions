104ea47e5637ef2832c2dd09d392612d5e49db71
Surepic <46145992+ioprojecton@users.noreply.github.com>
2021-08-14 23:05:45 -0400

    Update 8.c

--- 13/projects/08/8.c
+++ 13/projects/08/8.c
@@ -84,3 +84,54 @@ while(word[counter]!=NULL){
     }
 return sum;
 }
+
+///Solution2
+#include <stdio.h>
+#include <ctype.h>
+
+int compute_scrabble_value(const char *word){
+    int sum = 0;
+    while(*word) {
+                switch (toupper(*word++)) {
+                    case 'A': case 'E': case 'I': case 'L': case 'N': case 'O':
+                    case 'R': case 'S': case 'T': case 'U':
+                        sum++;
+                        break;
+                    case 'D': case 'G':
+                        sum += 2;
+                        break;
+                    case 'B': case 'C': case 'M': case 'P':
+                        sum += 3;
+                        break;
+                    case 'F': case 'H': case 'V': case 'W': case 'Y':
+                        sum += 4;
+                        break;
+                    case 'K':
+                        sum += 5;
+                        break;
+                    case 'J': case 'X':
+                        sum += 8;
+                        break;
+                    case 'Q': case 'Z':
+                        sum += 10;
+                        break;
+                    default:
+                        break;
+                }
+            
+    }
+    return sum;
+}
+int main(void) {
+
+    char c=0;
+
+    printf("Enter a word: ");
+    char my_array[20]={0};
+    
+    while ((my_array[c++] = getchar()) != '\n');
+    
+
+    printf("Scrabble value: %d\n", compute_scrabble_value(my_array));
+    return 0;
+}
