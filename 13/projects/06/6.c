e66d98c0b04321e00df3a902615bda4402dac5f9
Surepic <46145992+ioprojecton@users.noreply.github.com>
2021-08-14 22:28:31 -0400

    Update 6.c

--- 13/projects/06/6.c
+++ 13/projects/06/6.c
@@ -51,3 +51,32 @@ for(char *b=pl;b<pl+strlen(pl);b++)*b=toupper(*b);
     return 0;
 
 }
+
+//Solution2
+int main(int argc, char *argv[])
+{
+  char *planets[] = {"Mercury", "Venus", "Earth",
+                     "Mars", "Jupiter", "Saturn",
+                     "Uranus", "Neptune", "Pluto"};
+  int i, j;
+
+  for (i = 1; i < argc; i++) {
+      for (j = 0; j < NUM_PLANETS; j++){
+          char tmp[32]={0};
+          char pl[32]={0};
+          strcpy(tmp, argv[i]);
+          strcpy(pl, planets[j]);
+          for (char *b=tmp; *b; b++)*b=toupper(*b);
+          for (char *b=pl; *b; b++)*b=toupper(*b);
+          if (strcmp(tmp, pl) == 0) {
+            printf("%s is planet %d\n", argv[i], j + 1);
+            break;
+          }
+      }
+      
+    if (j == NUM_PLANETS)
+      printf("%s is not a planet\n", argv[i]);
+  }
+
+  return 0;
+}
