d0c155440d84b5fd534a3ffba724450a1ec926ef
Surepic <46145992+ioprojecton@users.noreply.github.com>
2021-08-14 22:04:58 -0400

    Update reverse.c

--- 13/projects/04/reverse.c
+++ 13/projects/04/reverse.c
@@ -9,3 +9,17 @@ int main(int argc, char *argv[]) {
     printf("\n");
     return 0;
 }
+
+///And with pointer
+
+int main(int argc, char *argv[])
+{
+    char **p;
+    for (p=&argv[argc-1]; p!=argv; p--) {
+        printf("%s ",*p);
+    }
+    putchar('\n');
+  
+  return 0;
+}
+
