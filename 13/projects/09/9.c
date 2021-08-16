671ecd0e170d1f1d211bc92b3fad4a45caf4555d
Surepic <46145992+ioprojecton@users.noreply.github.com>
2021-08-15 20:12:21 -0400

    Update 9.c

--- 13/projects/09/9.c
+++ 13/projects/09/9.c
@@ -23,3 +23,32 @@ for(char *p=sentence;*p!=NULL;p++) if(*p=='A' || *p=='E' || *p=='I' || *p=='O' |
 
 return vowels;
 }
+
+
+///Solution2
+#include <stdio.h>
+#include <ctype.h>
+
+int compute_vowel_count(const char *sentence){
+    int vowels = 0;
+    while (*sentence) {
+        switch (toupper(*sentence++)) {
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                vowels++;
+                break;
+            default:
+                break;
+        }
+    }
+    return vowels;
+}
+int main(void) {
+    printf("Enter a sentence: ");
+    char my_array[20] = {0};
+    char c = 0;
+    while ((my_array[c++] = getchar()) != '\n');
+    
+    
+    printf("Your sentence contains %d vowels.\n", compute_vowel_count(my_array));
+    return 0;
+}
