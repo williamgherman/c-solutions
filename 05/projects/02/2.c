963ebfa6340e7a87ff417c5f382c4b01197dc2e0
Null0x0zero <31679184+Buffer0overflow@users.noreply.github.com>
2022-01-16 18:57:05 +0300

    Fix error in logic

--- 05/projects/02/2.c
+++ 05/projects/02/2.c
@@ -4,15 +4,19 @@ int main(void) {
 
     int hour, minute;
 
-    printf("Enter a 24-hour time: ");
-    scanf("%d :%d", &hour, &minute);
-
-    if (hour < 11)
-        printf("Equivalent 12-hour time: %d:%d AM\n", 
-                hour == 0 ? 12 : hour, minute);
-    else 
-        printf("Equivalent 12-hour time: %d:%d PM\n", 
-               hour == 12 ? 12 : hour - 12, minute);
+  if ( hour > 12)
+    {
+        printf("Equivalent in 12 hour format is: %.2d:%.2d PM.",hour - 12, minute);
+    } else if ( hour == 0)
+    {
+       printf("Equivalent in 12 hour format is 12:%.2d AM.",minute);
+    } else if ( hour == 12)
+    {
+       printf("Equivalent in 12 hour format is: %.2d:%.2d PM.",hour, minute);
+    } else
+    {
+       printf("Equivalent in 12 hour format is: %.2d:%.2d AM.",hour, minute);   
+    };
 
     return 0;
 }
