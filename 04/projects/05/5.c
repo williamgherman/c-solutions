505ec34d3b3aeaa03c3bc1c05530fcb433903195
mehamednews <53986586+mehamednews@users.noreply.github.com>
2022-03-19 07:32:47 +0100

    Using arithmetic operators to solve 5.c

--- 04/projects/05/5.c
+++ 04/projects/05/5.c
@@ -1,21 +1,27 @@
-/* Computes a Universal Product Code check digit */
-
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
+	int n;
+	printf("Enter UPC: ");
+	scanf("%11d", &n);
+	int first_sum = (n % 10) +
+					((n % 1000) / 100) +
+					((n % 100000) / 10000) +
+					((n % 10000000) / 1000000) +
+					((n % 1000000000) / 100000000) +
+					((n % 100000000000) / 10000000000);
 
-    int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
-        first_sum, second_sum, total;
+	int second_sum =
+		((n % 100) / 10) +
+		((n % 10000) / 1000) +
+		((n % 1000000) / 100000) +
+		((n % 100000000) / 10000000) +
+		((n % 10000000000) / 1000000000);
 
-    printf("Enter the first 11 digits of a UPC: ");
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
-          &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+	int total = 3 * first_sum + second_sum;
 
-    first_sum = d + i2 + i4 + j1 + j3 + j5;
-    second_sum = i1 + i3 + i5 + j2 + j4;
-    total = 3 * first_sum + second_sum;
+	printf("Check digit: %d\n", 9 - (total - 1) % 10);
 
-    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
-
-    return 0;
+	return 0;
 }
