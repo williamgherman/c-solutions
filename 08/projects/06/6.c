9bdc2354fd0d6b06f1b59f9b536f89bbde360036
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-11 12:57:37 -0400

    Update 6.c
    another solution

--- 08/projects/06/6.c
+++ 08/projects/06/6.c
@@ -43,3 +43,37 @@ int main(void) {
 
     return 0;
 }
+
+//How about this?
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#define MAX_SIZE 100
+int main(void)
+{
+
+char message[MAX_SIZE]={0};
+
+printf("Enter message: ");
+
+char c;
+
+unsigned char counter=0;
+
+while((c=toupper(getchar()))!='\n' && counter<MAX_SIZE){
+
+switch(c){
+case 'A': message[counter++]='4';break;
+case 'B': message[counter++]='8';break;
+case 'E': message[counter++]='3';break;
+case 'I': message[counter++]='1';break;
+case 'O': message[counter++]='0';break;
+case 'S': message[counter++]='5';break;
+default: message[counter++]=c;break;
+}
+
+}
+printf("Translated message: %s!!!!!!!!!!\n",message);
+   return 0;
+}
