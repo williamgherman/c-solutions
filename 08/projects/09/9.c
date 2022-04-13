69a3b6d4d0e030f5952d88f334c654002fa07c68
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-13 10:57:16 -0400

    Update 9.c
    cleanup

--- 08/projects/09/9.c
+++ 08/projects/09/9.c
@@ -22,40 +22,40 @@ bool error=false;
 unsigned char r=0;
 unsigned char c=0;
 
-for(char letter='A';letter<='Z' && !error;letter++){
+for(char letter='A';letter<='Z' && !error;){
 
 unsigned char move=rand()%4;
 
 switch(move){
 case UP:
-    if((r+1<ROWS) && (array[r+1][c]=='.')) array[++r][c]=letter;
-    else if((r-1>=0) && (array[r-1][c]=='.')) array[--r][c]=letter;
-    else if((c+1<COLS) && (array[r][c+1]=='.')) array[r][++c]=letter;
-    else if((c-1>=0) && (array[r][c-1]=='.')) array[r][--c]=letter;
+    if(r+1<ROWS && array[r+1][c]=='.') array[++r][c]=letter++;
+    else if(r-1>=0 && array[r-1][c]=='.') array[--r][c]=letter++;
+    else if(c+1<COLS && array[r][c+1]=='.') array[r][++c]=letter++;
+    else if(c-1>=0 && array[r][c-1]=='.') array[r][--c]=letter++;
     else error=!error;
 break;
 
 case DOWN:
-    if((r-1>=0) && (array[r-1][c]=='.')) array[--r][c]=letter;
-    else if((r+1<ROWS) && (array[r+1][c]=='.')) array[++r][c]=letter;
-    else if((c+1<COLS) && (array[r][c+1]=='.')) array[r][++c]=letter;
-    else if((c-1>=0) && (array[r][c-1]=='.')) array[r][--c]=letter;
+    if(r-1>=0 && array[r-1][c]=='.') array[--r][c]=letter++;
+    else if(r+1<ROWS && array[r+1][c]=='.') array[++r][c]=letter++;
+    else if(c+1<COLS && array[r][c+1]=='.') array[r][++c]=letter++;
+    else if(c-1>=0 && array[r][c-1]=='.') array[r][--c]=letter++;
     else error=!error;
 break;
 
 case RIGHT:
-    if((c+1<COLS) && (array[r][c+1]=='.')) array[r][++c]=letter;
-    else if((c-1>=0) && (array[r][c-1]=='.')) array[r][--c]=letter;
-    else if((r+1<ROWS) && (array[r+1][c]=='.')) array[++r][c]=letter;
-    else if((r-1>=0) && (array[r-1][c]=='.')) array[--r][c]=letter;
+    if(c+1<COLS && array[r][c+1]=='.') array[r][++c]=letter++;
+    else if(c-1>=0 && array[r][c-1]=='.') array[r][--c]=letter++;
+    else if(r+1<ROWS && array[r+1][c]=='.') array[++r][c]=letter++;
+    else if(r-1>=0 && array[r-1][c]=='.') array[--r][c]=letter++;
     else error=!error;
 break;
 
 case LEFT:
-    if((c-1>=0) && (array[r][c-1]=='.')) array[r][--c]=letter;
-    else if((c+1<COLS) && (array[r][c+1]=='.')) array[r][++c]=letter;
-    else if((r+1<ROWS) && (array[r+1][c]=='.')) array[++r][c]=letter;
-    else if((r-1>=0) && (array[r-1][c]=='.')) array[--r][c]=letter;
+    if(c-1>=0 && array[r][c-1]=='.') array[r][--c]=letter++;
+    else if(c+1<COLS && array[r][c+1]=='.') array[r][++c]=letter++;
+    else if(r+1<ROWS && array[r+1][c]=='.') array[++r][c]=letter++;
+    else if(r-1>=0 && array[r-1][c]=='.') array[--r][c]=letter++;
     else error=!error;
 
 break;
@@ -69,6 +69,5 @@ for(unsigned char i=0,b=0;i<ROWS;i++,b=0){
     while(b<COLS) putchar(array[i][b++]);
     putchar('\n');
     }
-
     return 0;
 }
