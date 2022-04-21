078b9f4dc70ea6e43ac884f1f4a19725e3624df2
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-21 18:48:11 -0400

    Update 6.c
    some optimizations

--- 10/projects/06/6.c
+++ 10/projects/06/6.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
-
 #include <stdlib.h> /* exit */
-
 #include <stdbool.h> /* C99+ only */
-
-
+#include <ctype.h>
 
 #define STACK_SIZE 100
-
-
-
-/* external variables */
 
 char contents[STACK_SIZE] = {0};
 
 int top = 0;
-
-
 
 void make_empty(void);
 
@@ -31,8 +22,6 @@ char pop(void);
 void stack_overflow(void);
 
 void stack_underflow(void);
-
-
 
 int main(void) {
 
@@ -55,84 +44,44 @@ while((ch=getchar())!='\n'){
       push(b/a);
         }
         else if(ch=='='){
-            printf("%d\n Enter new one",pop());
+            printf("\nvalue of expression: %d\n",pop());
         }
 
               }}
     return 0;
-
 }
 
 void make_empty(void) {
-
     top = 0;
-
 }
-
-
 
 bool is_empty(void) {
-
     return top == 0;
-
 }
-
-
 
 bool is_full(void) {
-
     return top == STACK_SIZE;
-
 }
-
-
 
 void push(int i) {
-
-
-
-    if (is_full())
-
-        stack_overflow();
-
-    else
-
-        contents[top++] = i;
-
+    if (is_full()) stack_overflow();
+    else contents[top++] = i;
 }
-
-
 
 char pop(void) {
-
-
-
-    if (is_empty())
-
+    if (is_empty()){
         stack_underflow();
-
-    else
-
-        return contents[--top];
-
+        return 0;
+        }
+    else return contents[--top];
 }
-
-
 
 void stack_overflow(void) {
-
     printf("\nExpression is too complex\n");
-
     exit(EXIT_FAILURE);
-
 }
 
-
-
 void stack_underflow(void) {
-
     printf("\nNot enough operands in expression\n");
-
     exit(EXIT_FAILURE);
-
 }
