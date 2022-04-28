9836ba73077e5c6096b91ae4db97b527ff83111b
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-27 21:43:58 -0400

    Update 6.c
    added another solution

--- 13/projects/06/6.c
+++ 13/projects/06/6.c
@@ -1,3 +1,30 @@
+//this solutions works only under linux
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_PLANETS 9
+
+int main(int argc, char *argv[])
+{
+  char *planets[] = {"Mercury", "Venus", "Earth",
+                     "Mars", "Jupiter", "Saturn",
+                     "Uranus", "Neptune", "Pluto"};
+  int i, j;
+
+  for (i = 1; i < argc; i++) {
+    for (j = 0; j < NUM_PLANETS; j++)
+      if (strcasecmp(argv[i], planets[j]) == 0) { //strcasecmp linux function
+        printf("%s is planet %d\n", argv[i], j + 1);
+        break;
+      }
+    if (j == NUM_PLANETS)
+      printf("%s is not a planet\n", argv[i]);
+  }
+
+  return 0;
+}
+
+
 #include <stdio.h>
 
 #include <string.h>
