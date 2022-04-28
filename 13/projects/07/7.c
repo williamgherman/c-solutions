a5b0c7d060e0a1a3772d288795d6f97a78437fe9
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-27 22:07:50 -0400

    Update 7.c
    solution with ternery

--- 13/projects/07/7.c
+++ 13/projects/07/7.c
@@ -12,15 +12,43 @@ char* pointers[]={"one","two","three","four","five","six","seven","eight","nine"
 while(1){
     printf("Enter a two-digit number: ");
 
+    scanf("%2d", &n);
+if(n<10 || n>99) {
+        printf("out of range\n");
+continue;
+}
+
+if(n<=20) printf("%s\n",pointers[--n]);
+
+else printf("%s %s\n",pointers[n/10+18-1],pointers[n%10-1]);
+
+}
+    return 0;
+
+}
+
+//with ternery
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int n;
+char* pointers[]={"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve", \
+"thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","thirty","fourty","fifty","sixty",\
+"seventy","eighty","ninty"
+};
+
+while(1){
+    printf("Enter a two-digit number: ");
+
     scanf("%d", &n);
 if(n<10 || n>99) {
         printf("out of range\n");
 continue;
 }
 
-if(n<=20) printf("%s\n",pointers[n-1]);
-
-else printf("%s %s\n",pointers[n/10+18-1],pointers[n%10-1]);
+printf(n<=20?"%s\n":"%s %s\n",n<=20?pointers[n-1]:pointers[n/10+18-1],pointers[n%10-1]);
 
 }
     return 0;
