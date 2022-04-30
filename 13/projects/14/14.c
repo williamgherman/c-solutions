5d27a5c8cce45eab48c535e2cd3390438701a384
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-29 20:16:46 -0400

    Update 14.c
    added another solution

--- 13/projects/14/14.c
+++ 13/projects/14/14.c
@@ -46,3 +46,50 @@ for(unsigned char ch=0;ch<sizeof(table)/sizeof(table[0]);ch++) if(table[ch]) ret
 return true;
 
 }
+//solution 2
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool are_anagrams(const char *word1, const char *word2);
+
+bool are_anagrams(const char *word1, const char *word2){
+
+if(strlen(word1)!=strlen(word2)) return false;
+short sum=0;
+
+while(isalpha(*word1))sum+=*word1++;
+printf("sum=%hd",sum);
+while(isalpha(*word2))sum-=*word2++;
+printf("sum=%hd",sum);
+
+return !sum?true:false;
+
+}
+
+#define SIZE 26
+
+int main(void) {
+
+char first_word[SIZE]={0};
+char second_word[SIZE]={0};
+
+unsigned char i=0;
+
+printf("Enter first word: ");
+
+while((first_word[i++]=getchar())!='\n');
+
+printf("u enterered %s",first_word);
+
+printf("Enter second word: ");
+i=0;
+
+while((second_word[i++]=getchar())!='\n');
+
+printf("words are");
+are_anagrams(first_word,second_word)?puts("anagrams"):puts("not anagrams");
+
+return 0;
+}
