452b47d47a5ab3f932849d3ee2eeaabc4d2e01c3
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-04-30 21:44:17 -0400

    Update 17.c
    added another solution

--- 13/projects/17/17.c
+++ 13/projects/17/17.c
@@ -51,3 +51,42 @@ bool is_palindrome(const char *message) {
     while (message < q) if (toupper(*message++) != toupper(*q--)) return false; //no need to check toupper when putting into array in main
     return true;
 }
+//solution3
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool is_palindrome(const char *message);
+
+bool is_palindrome(const char *message){
+
+char temp[strlen(message)+1];
+
+memset(temp,'\0',sizeof(temp));
+
+for(unsigned char i=0,b=0;i<strlen(message);isalpha(message[i])?temp[b++]=toupper(message[i++]):i++);
+
+char *p=temp;
+char *q=&temp[strlen(temp)-1];
+
+while(p<q && *p++==*q--);
+
+return !(p-q);
+}
+
+#define SIZE 30
+
+int main(void) {
+    printf("Enter a message: ");
+    char my_array[SIZE] = {0};
+    
+    char *p = my_array;
+    
+    while ((*p=getchar())!='\n' && p++<my_array+SIZE);
+    *p='\0';
+    
+printf("array is ");
+is_palindrome(my_array)?puts("polidrome"):puts("not polidrome");
+    return 0;
+}
