d65e4fc6294ce010962f156b9196cd2a24e291ce
Surepic <46145992+ioprojecton@users.noreply.github.com>
2022-06-01 14:09:43 -0400

    added another solution

--- 23/projects/02/2c1.c
+++ 23/projects/02/2c1.c
@@ -17,3 +17,27 @@ while((c=getchar())!=EOF){
    return(0);
 }
 
+### another solution
+int main(int argc,char *argv[])
+{
+
+FILE *fp;
+
+if(argc!=2) fprintf(stderr,"file not provided or more arguments");
+
+if(!(fp=fopen(argv[argc-1],"r"))) fprintf(stderr,"cant open file %s",argv[argc-1]);
+
+char str[100]={0};
+unsigned char counter=0;
+
+while(fgets(str,sizeof(str),fp)){
+
+while(isspace(str[counter])) counter++;
+
+while(putchar(str[counter]) && counter++<strlen(str));
+counter=0;
+}
+
+fclose(fp);
+  return 0;
+}
